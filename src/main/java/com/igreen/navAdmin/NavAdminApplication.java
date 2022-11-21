d89f3d8dfdb7f5a8b5868108dae79c395069d997
ragu <ragubaran.thevarasah@tsgforce.com>
2022-11-21 13:55:54 +1100

    first commit

---
+++ src/main/java/com/igreen/navAdmin/NavAdminApplication.java
@@ -0,0 +1,13 @@
+package com.igreen.navAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igreen/navAdmin/NavAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igreen.navAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
